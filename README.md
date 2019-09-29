3f76a7ec949be352bd36393bb21361d111e28d0b
rob.treharne <rob.treharne@gmail.com>
2019-09-29 16:24:07 +0100

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction to R
+
+R. Treharne
+
+This repository contains material delivered as part of LIFE113 and LIFE223 Quantitative Skills modules in the School of Life Sciences, University of Liverpool.
--- intro.R
+++ intro.R
@@ -25,12 +25,15 @@
 #   + How many columns are there?
 #   + Do you know what each column refers to?
 
-math <- read.csv("student-mat.csv")
+math <- read.csv("student-mat.csv") # you might need to use sep=";"
+# math <- read.csv("student-mat.csv", sep=";")
 
 # STEP 3:
 #  + Consider the G3 test scores (end of year score)
 #  + Use R to generate a histogram of the G3 test scores (save the graph in your cwd)
 #  + What should we do about all the "zero" scores?
+
+help(hist)
 
 hist(math$G3, xlab="G3 test score", main="", col="grey")
 
@@ -39,7 +42,7 @@ hist(math$G3, xlab="G3 test score", main="", col="grey")
 #  + Can you remove these scores from the dataset and re-plot the bar chart?
 
 math_nozeroG3 <- math[which(math$G3 > 0),] # Create a new data frame using conditional statement
-hist(math_nozeroG3$G3, xlab="G3 test score (excluding zero scores", main="", col="grey")
+hist(math_nozeroG3$G3, xlab="G3 test score (excluding zero scores)", main="", col="red") # Plot a histogram of the G3 scores in the new dataframe
 
 # STEP 5:
 #  + Does this distribution look normal to you?
@@ -47,17 +50,36 @@ hist(math_nozeroG3$G3, xlab="G3 test score (excluding zero scores", main="", col
 #  + Run help("shapiro.test") and see what happens
 #  + Do G3 test scores (try with and without zero scores) approximate a normal distribution?
 
-help("shapiro.test")
-shapiro.test(math$G3)
-shapiro.test(math_nozeroG3$G3)
+help("shapiro.test") # run help on any function to find out more info. RStudio is brill for this reason.
+shapiro.test(math$G3) # run the Shapiro normality test on G3 scores.
+shapiro.test(math_nozeroG3$G3) # run the Shapiro normality test on G3 scores with zero scores removed.
 
 # STEP 6: 
 #  + Consider the G3 test scores of female (F) and male (M) groups
-#  + Use R to create a box plot for the two groups (include zero scores) (Save to your cwd)
+#  + Use R to create a box plot for the two groups (exclude zero scores) (Save to your cwd)
 #  + Which group do you think performed better by the look of it?
+#  + Can you create two histograms for each group (M & F) and overlay them on the same plot (Save to your cwd)
 #  + Construct a null hypothesis
 
-boxplot(G3~sex, data=math, xlab="Sex", ylab="G3 Score")
+boxplot(G3~sex, data=math_nozeroG3, xlab="Sex", ylab="G3 Score")
+
+F_G3 = math_nozeroG3[which(math_nozeroG3$sex=="F"), ]$G3
+M_G3 = math_nozeroG3[which(math_nozeroG3$sex == "M"), ]$G3
+
+# Plotting an overlaid histogram
+
+h1 <- hist(F_G3) # histogram for Females only
+h2 <-  hist(M_G3) # histogram for Males only
+
+col_F <- rgb(0,1,1,1/4) # Define a colour for Female bars
+col_M <- rgb(1,0,1,1/4) # Define a colour for Male bars
+
+plot(h1, col=col_F) # Create a plot "canvas" and add h1 histogram to it
+plot(h2, col=col_M, add=T, xlab="G3 Test Scores (excluding zeros)") # Add h2 histogram using "add=T" parameter. Don't forget to label axes.
+
+legend("topright", c("Male", "Female"), fill=c(col_M, col_F)) # This is how you add a legend. All graphs should have one of these
+
+# OK. Based on the boxplots and the histogram let's construct a hypotheses.
 
 # H_0: There is no significance in the difference between the mean G3 scores of females and males (in Portuguese secondary schools)
 # H_A: There is a significance in the difference between the mean G3 scores ...
@@ -70,18 +92,16 @@ boxplot(G3~sex, data=math, xlab="Sex", ylab="G3 Score")
 #  + Does it matter if scores within each group approximate a normal distribution?
 #  + Run help("t.test") and see what happens
 
-group_F_G3 = math[which(math$sex=="F"), ]$G3
-group_M_G3 = math[which(math$sex == "M"), ]$G3
 
-shapiro.test(group_F_G3)
-shapiro.test(group_M_G3)
+shapiro.test(F_G3)
+shapiro.test(M_G3)
 
 help("t.test")
 
 # + Perform an independent (unpaired) T-test on the F and M data
 # + Use the p-value from the test to determine whether to accept/recject your hypotheses.
 
-t.test(group_M_G3, group_F_G3)
+t.test(M_G3, F_G3)
 
 # p < 0.05: Reject H_0. Accept H_A. There is a significant difference ...
 
@@ -94,3 +114,27 @@ t.test(group_M_G3, group_F_G3)
 #  + Construct a null hypothesis based on two variables (e.g. sex and score)
 #  + Test your hypothesis using a T-test
 #  + Intepret the p-value from the test to decide whether to accept/reject your hypothesis?
+
+# EXERCISES:
+
+# Exercise 1 (for week 2 workshop)
+
+
+boxplot(G3~Walc, data=math, xlab="In a romantic relationship", ylab="G3 Score")
+hist(math$Walc)
+
+group_1 = math_nozeroG3[which(math_nozeroG3$Walc <3), ]$G3 # Students who score less than 3 on weekly alcohol rating
+group_2 = math_nozeroG3[which(math_nozeroG3$Walc >2), ]$G3 # Students who score less than 2 on weekly alcohol rating
+
+a <- hist(group_1, col='red')
+b <- hist(group_2, col='blue')
+
+colour_1 = rgb(1,0,0,0.3)
+colour_2 = rgb(0,0,1,0.3)
+
+plot(a, col=colour_1, xlab="G3 Test Score")
+plot(b, ,col=colour_2, add=T)
+
+t.test(group_1, group_2)
+
+help(plot)
